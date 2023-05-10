aeef4c16daf385486a2af2e45235b33d5ce1ecf0
hassamarif <arifhassam18@gmail.com>
2023-05-11 01:31:06 +0500

    updates pushed

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import Chat from "./screens/Chat";
 import Login from "./screens/Login";
+import Signup from "./screens/Signup";
 
 const Stack = createStackNavigator();
 
 function ChatStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
     </Stack.Navigator>
   );
 }
